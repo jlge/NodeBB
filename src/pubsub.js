3b466c6707f3cb10708c73f32358d5f90c2b2169
Joanna Ge <joannage87@gmail.com>
2023-01-25 19:00:46 -0500

    Add PubSub class extending EventEmitter and change relevant variable/type assignments

--- src/pubsub.js
+++ src/pubsub.js
@@ -9,6 +9,8 @@ const nconf_1 = __importDefault(require("nconf"));
 let real;
 let noCluster;
 let singleHost;
+class PubSub extends events_1.default {
+}
 function get() {
     if (real) {
         return real;
@@ -19,7 +21,7 @@ function get() {
             real = noCluster;
             return real;
         }
-        noCluster = new events_1.default();
+        noCluster = new PubSub();
         noCluster.publish = noCluster.emit.bind(noCluster);
         pubsub = noCluster;
     }
@@ -28,13 +30,14 @@ function get() {
             real = singleHost;
             return real;
         }
-        singleHost = new events_1.default();
+        singleHost = new PubSub();
         if (!process.send) {
             singleHost.publish = singleHost.emit.bind(singleHost);
         }
         else {
             singleHost.publish = function (event, data) {
-                process.send({
+                var _a;
+                (_a = process.send) === null || _a === void 0 ? void 0 : _a.call(process, {
                     action: 'pubsub',
                     event: event,
                     data: data,
@@ -49,6 +52,9 @@ function get() {
         pubsub = singleHost;
     }
     else if (nconf_1.default.get('redis')) {
+        // The next line calls a module that has not been updated to TS yet
+        // My code should be able to be extended to the following file though!
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         pubsub = require('./database/redis/pubsub');
     }
     else {
@@ -61,6 +67,8 @@ function publish(event, data) {
     get().publish(event, data);
 }
 exports.publish = publish;
+// The on() function in EventEmitter class defines the callback with type (...args: any[]) => void)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function on(event, callback) {
     get().on(event, callback);
 }
--- src/pubsub.ts
+++ src/pubsub.ts
@@ -1,42 +1,47 @@
 import EventEmitter from 'events';
 import nconf from 'nconf';
 
-let real;
-let noCluster;
-let singleHost;
+let real : null|PubSub;
+let noCluster : PubSub;
+let singleHost : PubSub;
+
+class PubSub extends EventEmitter {
+    publish: (event : string | symbol, data : object) => void;
+}
+
 
 interface Message {
     action: string,
     event : string | symbol,
-    data: unknown
+    data: object
 }
 
-function get() {
+function get(): PubSub {
     if (real) {
         return real;
     }
 
-    let pubsub : EventEmitter;
+    let pubsub : PubSub;
 
     if (!nconf.get('isCluster')) {
         if (noCluster) {
             real = noCluster;
             return real;
         }
-        noCluster = new EventEmitter();
-        noCluster.publish = noCluster.emit.bind(noCluster);
+        noCluster = new PubSub();
+        noCluster.publish = noCluster.emit.bind(noCluster) as PubSub['publish'];
         pubsub = noCluster;
     } else if (nconf.get('singleHostCluster')) {
         if (singleHost) {
             real = singleHost;
             return real;
         }
-        singleHost = new EventEmitter();
+        singleHost = new PubSub();
         if (!process.send) {
-            singleHost.publish = singleHost.emit.bind(singleHost);
+            singleHost.publish = singleHost.emit.bind(singleHost) as PubSub['publish'];
         } else {
-            singleHost.publish = function (event : string | symbol, data : unknown) {
-                process.send({
+            singleHost.publish = function (event : string | symbol, data : object): void {
+                process.send?.({
                     action: 'pubsub',
                     event: event,
                     data: data,
@@ -50,6 +55,9 @@ function get() {
         }
         pubsub = singleHost;
     } else if (nconf.get('redis')) {
+        // The next line calls a module that has not been updated to TS yet
+        // My code should be able to be extended to the following file though!
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         pubsub = require('./database/redis/pubsub');
     } else {
         throw new Error('[[error:redis-required-for-pubsub]]');
@@ -59,15 +67,17 @@ function get() {
     return pubsub;
 }
 
-export function publish(event : string | symbol, data: unknown) {
+export function publish(event: string | symbol, data: object) {
     get().publish(event, data);
 }
 
-export function on(event, callback) {
+// The on() function in EventEmitter class defines the callback with type (...args: any[]) => void)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function on(event: string | symbol, callback: (...args: any[]) => void) {
     get().on(event, callback);
 }
 
-export function removeAllListeners(event) {
+export function removeAllListeners(event: string | symbol) {
     get().removeAllListeners(event);
 }
 
